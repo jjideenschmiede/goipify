5e482147fe0089ee271f0ef95e48cfe979fc7229
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-12-06 14:11:35 +0100

    Add main function

--- ip.go
+++ ip.go
@@ -10,3 +10,58 @@
 //********************************************************************************************************************//
 
 package goipify
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+const (
+	transferProtocol = "https://"
+	baseUrl          = "api.ipify.org"
+)
+
+// Return is to decode the json data
+type Return struct {
+	Ip string `json:"ip"`
+}
+
+// Ip is to get your ip address
+func Ip() (Return, error) {
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest("GET", transferProtocol+baseUrl, nil)
+	if err != nil {
+		return Return{}, err
+	}
+
+	// Parse url & add attributes
+	parse := request.URL.Query()
+	parse.Add("format", "json")
+	request.URL.RawQuery = parse.Encode()
+
+	// Define header & check if access token is active
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return Return{}, err
+	}
+
+	// Decode data
+	var decode Return
+
+	err = json.NewDecoder(response.Body).Decode(&decode)
+	if err != nil {
+		return Return{}, err
+	}
+
+	// Return data
+	return decode, err
+
+}
